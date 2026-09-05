94217014623f62cdb0a8d5a52d7e4beb0b43cc0a
donovangong <gongchenghan@gmail.com>
2026-09-05 23:34:39 +0100

    Comment out specific search terms in job_logic.py
    Comment out entry level, junior, and trainee search terms.

--- job_logic.py
+++ job_logic.py
@@ -10,9 +10,9 @@ from jobspy import scrape_jobs
 
 SEARCH_TERMS = [
     "graduate",
-    "entry level",
-    "junior",
-    "trainee",
+    # "entry level",
+    # "junior",
+    # "trainee",
     "new grad"
 ]
 
